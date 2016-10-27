b7478d6f00154ce62c6e62d23311694c0d3dcdc1
James Cool <jeffreysbrother@gmail.com>
2016-10-26 22:26:07 -0700

    added  some props

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import Title from "./Header/Title";
+
 export default class Header extends React.Component {
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+
   render() {
     return (
-      <header>header</header>
+      <div>
+        <Title title={this.props.title} />
+        <input value={this.props.title} onChange={this.handleChange.bind(this)} />
+      </div>
     );
   }
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,10 +4,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Welcome",
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({title});
+  }
+
   render() {
     return (
       <div>
-        <Header />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
         <Footer />
       </div>
     );
