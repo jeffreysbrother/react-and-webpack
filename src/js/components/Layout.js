95e45c26823710da0c403bcc13fa0547eff1c650
James Cool <james.cool@thecontrolgroup.com>
2017-07-07 15:55:09 -0700

    state and setTimeout

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = {name: "james"};
+	}
 	render() {
+		setTimeout(() =>{
+			this.setState({name: "Bob"});
+		}, 1000);
 		return (
 			<div>
+			{this.state.name}
 				<Header />
 				<Footer />
 			</div>
