538c6c2a43757ae61373c44a56b5ac118b9349bd
James Cool <james.cool@thecontrolgroup.com>
2017-07-07 15:38:01 -0700

    import and export

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,26 +1,13 @@
 import React from "react";
 
-import Header from "./Header";
-import Footer from "./Footer";
-
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      title: "Welcome",
-    };
-  }
-
-  changeTitle(title) {
-    this.setState({title});
-  }
-
-  render() {
-    return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
-        <Footer />
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.name = "james";
+	}
+	render() {
+		return (
+			<h1>It's {this.name}!</h1>
+		);
+	}
 }
